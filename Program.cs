5f05309588bd1a9346b6c1325711496b9baaa052
Ari Ketola <ariketola@hotmail.com>
2023-02-23 10:59:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Lander
+{
+
+    class Land
+    {
+
+        public string namn, styrestyp, huvudstad, invånarantal;
+      
+
+
+        public void Print()
+        {
+            Console.WriteLine("Landet: " + namn + "  " + LastName);
+            Console.WriteLine($"Telefon:   {Phone}   ");
+            Console.WriteLine("Adress:  " + Adress);
+        }
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+   
+    
+    
+    
+    
+    
+    }
+}
